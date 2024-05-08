e52079d309d435ca7e9ceb9c8cd71353b38b6c89
Gustavo <crymerom@gmail.com>
2024-05-08 07:20:58 +0000

    Revert "Merge pull request #21 from crymE-L/mf/rework-nextjs-project"
    This reverts commit 9136e6a0cc2444b26550ca707d2033e974eac8cd, reversing
    changes made to eaedc2b7bdb598b6bc6a4399ce1ca6b1e90dbf49.
    
    Reverse commit

--- frontend/components/custom/PersonalDataChunk/page.tsx
+++
@@ -1,6 +0,0 @@
-export default function PersonalDataChunk({ title: string, children: any }) {
-  <div className="space-y-2">
-    <p className="text-lg font-semibold">{title}</p>
-    {children}
-  </div>;
-}
